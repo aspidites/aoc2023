727a33684a734b93c339f538b3918dee26687bab
Edwin Marshall <emarshall85@gmail.com>
2023-12-02 21:31:51 -0800

    Reorganized some files

--- app/Cli.hs
+++ app/Cli.hs
@@ -1,7 +1,18 @@
 {-# LANGUAGE DataKinds #-}
-module Cli ( Config(..) ) where
+module Cli ( Config(..), run ) where
 
+import AoC.Day1 qualified as Day1
+import AoC.Day2 qualified as Day2
+import Data.Aeson
+import Data.ByteString.Lazy.Char8 qualified as BS
 import Options.Generic
+
+
+solutions :: [(Int, (String -> Int, String -> Int))]
+solutions = 
+  [ (1, ( Day1.part1, Day1.part2 ) )
+  , (2, ( Day2.part1, Day2.part2 ) )
+  ]
 
 data Config w = Config
   { day :: w ::: Maybe Int <?> "Which day to run solutions for"
@@ -11,3 +22,26 @@ data Config w = Config
 
 instance ParseRecord (Config Wrapped)
 deriving instance Show (Config Unwrapped)
+
+run :: Maybe Int -> Maybe String -> Maybe Bool -> IO ()
+run Nothing _ _ = putStrLn "Please specify which day's solutions you want to run as an integer"
+run _ Nothing _ = putStrLn "Please specify an input file to run solutions against"
+run (Just d) (Just i) json = do
+  input <- readFile i
+
+  case lookup d solutions of
+    Nothing -> putStrLn "not implemented"
+    Just (part1, part2) -> do
+      let partOne = part1 input
+          partTwo = part2 input
+      
+      case json of
+        Just True -> do
+          BS.putStrLn $ encode $ object 
+            [ "part_one" .= partOne
+            , "part_two" .= partTwo
+            ]
+        _ -> do
+          print partOne
+          print partTwo
+
--- app/Main.hs
+++ app/Main.hs
@@ -2,42 +2,10 @@
 module Main where
 
 import Control.Applicative
-import AoC.Day1 qualified as Day1
-import AoC.Day2 qualified as Day2
 
-import Data.ByteString.Lazy.Char8 qualified as BS
 import Cli
 import Options.Generic
 import System.Environment
-import Data.Aeson
-
-solutions :: [(Int, (String -> Int, String -> Int))]
-solutions = 
-  [ (1, ( Day1.part1, Day1.part2 ) )
-  , (2, ( Day2.part1, Day2.part2 ) )
-  ]
-
-run :: Maybe Int -> Maybe String -> Maybe Bool -> IO ()
-run Nothing _ _ = putStrLn "Please specify which day's solutions you want to run as an integer"
-run _ Nothing _ = putStrLn "Please specify an input file to run solutions against"
-run (Just d) (Just i) json = do
-  input <- readFile i
-
-  case lookup d solutions of
-    Nothing -> putStrLn "not implemented"
-    Just (part1, part2) -> do
-      let partOne = part1 input
-          partTwo = part2 input
-      
-      case json of
-        Just True -> do
-          BS.putStrLn $ encode $ object 
-            [ "part_one" .= partOne
-            , "part_two" .= partTwo
-            ]
-        _ -> do
-          print partOne
-          print partTwo
 
 main :: IO ()
 main = do
